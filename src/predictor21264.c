559c0bc1fa652f13a666e910db27114d798307d4
William Fong <trigition@gmail.com>
2014-11-28 02:14:13 -0700

    Started to create tournament predictor for Alpha 21264 predictor.

--- src/predictor21264.c
+++ src/predictor21264.c
@@ -1,15 +1,105 @@
 // A very stupid predictor.  It will always predict not taken.
+#include "predictor21264.h"
+
+localTable localHistory;
+predictionTable localPrediction;
+
+gPredictionTable globalPrediction;
+chooserTable cPredictionTable;
 
 void init_predictor ()
 {
-
+	int i;
+	globalHistory = 3;
+	for (i = 0; i < 1024; i++) {
+		localHistory.table[i] = 0;
+		localPrediction.table[i] = 0;
+		globalPrediction.table[i] = 0;
+		cPredictionTable.table[i] = 0;
+	}
+	for (; i < 4096; i++) {
+		globalPrediction.table[i] = 0;
+		cPredictionTable.table[i] = 0;
+	}
+	//printf("Successfully set everything to 0\n");
 }
 
 bool make_prediction (unsigned int pc)
 {
+	//Retrieve lowest 10 bits from PC
+	unsigned int bitMask = 0x3FF;
+	unsigned short index = pc & bitMask;
+	printf("PC: %u Index: %hu\n", pc, index);
+	//Determine to use global or local predictors
+	if (cPredictionTable.table[index] <= 1) {
+		//Use Local Table
+		return local_prediction(index);
+	} else {
+		//Use Global Table
+	}
   return false;
 }
 
 void train_predictor (unsigned int pc, bool outcome)
-{
+{	
+	if (outcome == false) {
+
+	} else if (outcome == true) {
+
+	}
+}
+
+
+bool local_prediction(unsigned short index) {
+	unsigned short predictIndex;
+	//Retrieve 10-bit local history and generate prediction index
+	predictIndex = localHistory.table[index];
+	//Determine if branch is taken or not
+	if (localPrediction.table[predictIndex] < 4) {
+		//Assume not taken
+		return false;
+	} else {
+		//Assume taken
+		return true;
+	}
+}
+
+void update_local_predictor (unsigned short index, bool outcome) {
+	//Update Prediction Table
+	unsigned short predictIndex = localHistory.table[index];
+	unsigned short prediction = localPrediction.table[predictIndex];
+	//Ensure that saturation values remain in defined domains (0-3)
+	if (prediction > 0 && prediction < 3) {
+		if (outcome) {
+			//Outcome was taken so increment local prediction saturation
+			localPrediction.table[predictIndex]++;
+		} else {
+			//Outcome was not taken so decrement local prediction saturation
+			localPrediction.table[predictIndex]--;
+		}
+	}
+
+	//Update Local History
+	//Shift history to push last outcome
+	localHistory.table[index] = (localHistory.table[index]) << 1;
+	if (outcome) {
+		//If outcome was true, set history bit
+		localHistory.table[index]++;
+	}
+}
+
+bool global_prediction () {
+	//Create index from 12 bit history
+	unsigned int index = globalHistory & 0xFFF;
+	//Retrieve saturation counter
+	if (globalPrediction.table[index] <= 1) {
+		//Not taken
+		return false;
+	} else {
+		return true;
+	}
+}
+
+void update_global_predictor () {
+	//TODO Update global predictor
 }
--- src/predictor21264.h
+++ src/predictor21264.h
@@ -1,6 +1,6 @@
 #ifndef PREDICTOR21264_H
 #define PREDICTOR21264_H
-
+#include <stdio.h>
 /*
   Define all your tables and their sizes here.
   All tables must be statically-sized.
@@ -13,46 +13,28 @@
   case the simulator will use more memory -- that's okay, we're only
   concerned about the memory used by the simulated branch predictor.
 */
-
-//Create 10-bit sizes for entries on the local history table
-typedef struct localEntry {
-	unsigned int entry:10;
-} lEntry;
-
 //Create 1024 entries for local history table
 typedef struct localHistory {
-	lEntry table[1024];
+	unsigned short table[1024];
 } localTable;
-
-//Create 3-bit sizes for prediction table entries
-typedef struct predictEntry {
-	unsigned int entry:3;
-} pEntry;
 
 //Create 1024 entries for local prediction table
 typedef struct localPredict {
-	pEntry table[1024];
+	char table[1024];
 } predictionTable;
 
 //Create Global History Table
-typedef struct globalHistory {
-	unsigned int history:12;
-} globalTable;
-
-//Create entry sizes for the global prediction table and the chooser prediction table
-typedef struct globalEntry {
-	unsigned int entry:2;
-} gEntry;
+unsigned int globalHistory;
 
 //Create Global Prediction Table
-typedef struct globalPrediction {
-	gEntry table[4096];
+typedef struct globalTable {
+	char table[4096];
 } gPredictionTable;
 
 //Create Chooser Prediction table
-typedef struct predictionTable {
-	gEntry table[4096];
-}
+typedef struct chooserPredictionTable {
+	char table[4096];
+} chooserTable;
 
 
 /*
@@ -73,5 +55,12 @@ bool make_prediction (unsigned int pc);
   indicates that the branch was not taken).
 */
 void train_predictor (unsigned int pc, bool outcome);
+
+bool local_prediction (unsigned short index);
+void update_local_predictor (unsigned short index, bool outcome);
+
+bool global_prediction ();
+void update_global_predictor ();
+
 
 #endif
