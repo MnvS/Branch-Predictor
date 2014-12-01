503faf44c8b2918f0b7da6013fe35ec6070d52fb
thegui <tguinard@nmt.edu>
2014-12-01 02:42:44 -0700

    added comments and changed to bitfields

--- src/predictororg.C
+++ src/predictororg.C
@@ -1,9 +1,14 @@
 #include "predictororg.h"
 
-gtable global_history;
-btable branch_history_table[16384];
+struct gtable global_history;
+struct btable branch_history_table[16384];
 
+/**
+ * Update global history and branch history table
+ */
 void train_predictor(unsigned int pc, bool outcome){
+	
+	//update branch history
 	unsigned index = (pc ^ global_history.val) & GLOBAL_MASK;
 	if(outcome && branch_history_table[index].counter < 3){
 		branch_history_table[index].counter += 1;
@@ -11,10 +16,16 @@ void train_predictor(unsigned int pc, bool outcome){
 	else if(!outcome && branch_history_table[index].counter > 0){
 		branch_history_table[index].counter -= 1;
 	}
+	
+	//update global history
 	global_history.val <<= 1;
 	global_history.val |= (unsigned) outcome;
 }
 
+/**
+ * Initialize predictor
+ * All counters are set to 0 (not taken)
+ */
 void init_predictor(){
 	global_history.val = 0;
 	int i;
@@ -23,6 +34,9 @@ void init_predictor(){
 	}
 }
 
+/**
+ * Give prediction of branch
+ */
 bool make_prediction(unsigned int pc){
 	unsigned index = (pc ^ global_history.val) & GLOBAL_MASK;
 	return branch_history_table[index].counter & 2;
